1663c5b2c5eaecd8943b750fb67092e9ae1baa25
Musicaljelly <musicaljelly@gmail.com>
2020-09-26 13:10:25 -0700

    Shuffled some stuff around, removed unneeded comment

--- hydrozoa-labs-backend/app.js
+++ hydrozoa-labs-backend/app.js
@@ -1,30 +1,9 @@
 const http = require('http');
 const fs = require('fs');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-
-/*
-let html = fs.readFileSync('../hydrozoa-labs/public/index.html');
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end(html);
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
-*/
-
-
-
-
-
 const express = require('express');
 const path = require('path');
+
+const port = 3000;
 
 const app = express();
 
