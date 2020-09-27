08eb7af2165e0101f54721df8d409021842dfb9c
João Lima <theotherlima@gmail.com>
2020-09-27 10:40:30 -0700

    ClassCard stubs

--- hydrozoa-labs/src/components/ClassCard.js
+++ hydrozoa-labs/src/components/ClassCard.js
@@ -1,5 +1,12 @@
 import React, { useState } from 'react'
+import Button from 'react-bootstrap/esm/Button';
+import Col from 'react-bootstrap/esm/Col';
+import Container from 'react-bootstrap/esm/Container';
+import Row from 'react-bootstrap/esm/Row';
 import Modal from 'react-bootstrap/Modal'
+import DropdownButton from 'react-bootstrap/DropdownButton'
+import Dropdown from 'react-bootstrap/esm/Dropdown';
+
 
 
 export default function ClassCard(props) {
@@ -10,12 +17,17 @@ export default function ClassCard(props) {
 
     const [show, setShow] = useState(1)
 
-    // const handleClose = () => setShow(false);
     function handleClose() {
         setShow(false);
         props.toggleModal('')
     }
-    // const handleOpen = () => setShow(true);
+
+    let students = "No students";
+    if (props.students) {
+        students = props.students.map((s) => {
+            return <Dropdown.Item>{s.name}</Dropdown.Item>
+        })
+    }
 
     return (
         <Modal show={show} onHide={handleClose}>
@@ -23,8 +35,43 @@ export default function ClassCard(props) {
                 <Modal.Title> {title}</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                Yay it's working?
+                <Container>
+                    <Row>
+                        <Col>
+                            Subject:
+                        </Col>
+                        <Col>
+                            PROPS.SUBJECT
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            Grade:
+                        </Col>
+                        <Col>
+                            PROPS.GRADE
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <DropdownButton title="Students">
+                                {students}
+                            </DropdownButton>
+                        </Col>
+                        <Col>
+                            <Button>Add Student</Button>
+                        </Col>
+
+                    </Row>
+                    <Row><Col sm={12}>
+                        <Button>Add Lesson</Button>
+                    </Col>
+                    </Row>
+                </Container>
             </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary">Close</Button>
+            </Modal.Footer>
         </Modal>
     )
 }
