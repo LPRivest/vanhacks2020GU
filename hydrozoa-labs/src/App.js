8b6bf575dee23192f0f1f3a991fdf641171ad13d
Jakob Manning <70977283+jakob-manning@users.noreply.github.com>
2020-09-27 10:16:27 -0700

    adding home page to app.js

--- hydrozoa-labs/src/App.js
+++ hydrozoa-labs/src/App.js
@@ -7,12 +7,14 @@ import { Switch, Route, Link } from 'react-router-dom'
 import Educator from './pages/Educator'
 import Teacher from './pages/Teacher'
 import Parent from './pages/Parent';
+import Student from "./pages/Student";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Nav name="Joao"></Nav>
+        <Nav></Nav>
         <Switch>
           <Route path="/educator">
             <Educator />
@@ -34,7 +36,29 @@ function App() {
                 teacher: "You",
                 lastUpdated: "Last Week"
               }
-            ]} />
+
+            ]}/>
+          </Route>
+          <Route path="/parent">
+            <Parent />
+          </Route>
+          <Route path="/student">
+            <Student studentProgress={[
+                {
+                    class: "Math A",
+                    course: "Mathematics",
+                    outcome: "Trigonometry",
+                    lesson: "Unit circle",
+                    status: "completed"
+                },
+                {
+                    class: "Math B",
+                    course: "Mathematics",
+                    outcome: "Trigonometry",
+                    lesson: "Special Angle Triangles",
+                    status: "completed"
+                }
+            ]}  />
           </Route>
           <Route path="/">
             <Home />
@@ -44,25 +68,6 @@ function App() {
     </BrowserRouter>
   );
 }
-function Home() {
-  function handleClick(e) {
-    var xhr = new XMLHttpRequest()
-    xhr.addEventListener('load', () => {
-      if (xhr.status == 200) {
-        alert(xhr.responseText)
-      }
-    })
-    // TODO: Don't query localhost!!!
-    xhr.open('POST', "http://localhost:3000/" + document.getElementById("endpointname").value)
-    xhr.setRequestHeader('Content-type', 'application/json')
-    xhr.send(document.getElementById("endpointbody").value)
-  }
-  return <> <h2>Home</h2>
-  Endpoint name: <input type="text" id="endpointname" /><br /><br />
-      JSON body: <input type="text" id="endpointbody" /><br /><br />
-    <button onClick={handleClick} style={{ marginTop: "40px" }}>Submit API request</button>
-  </>;
 
-}
 
 export default App;
