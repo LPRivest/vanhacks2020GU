4b90990ccdf67b80e7b016d14c3ae47a9ababcf2
Musicaljelly <musicaljelly@gmail.com>
2020-09-26 18:51:30 -0700

    First pass at CSV parsing

--- hydrozoa-labs/src/App.js
+++ hydrozoa-labs/src/App.js
@@ -2,7 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+var STATUS_OK = 200
+
 function App() {
+
+  function handleClick(e) {
+      var xhr = new XMLHttpRequest()
+      xhr.addEventListener('load', () => {
+          if (xhr.status == STATUS_OK){
+              alert(xhr.responseText)
+          }
+      })
+      // TODO: Don't query localhost!!!
+      xhr.open('POST', "http://localhost:3000")
+      xhr.send()
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +33,8 @@ function App() {
         >
           Learn React
         </a>
+
+        <button onClick={handleClick} style={{marginTop: "40px"}}>Send test request</button>
       </header>
     </div>
   );
