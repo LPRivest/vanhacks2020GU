c48a0c09aabc53da6b4d08ccba3950e30824d43b
João Lima <theotherlima@gmail.com>
2020-09-27 09:47:11 -0700

    Changed Educator to Teacher and made new Educator. Removed Student from Nav

---
+++ hydrozoa-labs/src/pages/Teacher.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+import Container from 'react-bootstrap/esm/Container'
+import Button from 'react-bootstrap/Button'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import PageTemplate from '../components/PageTemplate'
+import ClassCard from '../components/ClassCard'
+
+
+export default function Teacher(props) {
+    const [showModal, setShowModal] = useState(0)
+
+    function handleClassClick(clas) {
+        setShowModal(clas);
+    }
+
+
+    let classes = props.classes.map((clas) => {
+        return (
+            <Row><Col>
+                <ClassHelper
+                    class={clas.name}
+                    lastUpdated={clas.lastUpdated}
+                    onClick={handleClassClick}
+                />
+            </Col></Row>
+        )
+    })
+
+    let classModal = '';
+    if (showModal) {
+        classModal = <ClassCard title={showModal} toggleModal={handleClassClick} />
+    }
+
+    return (
+        <PageTemplate>
+            {classes}
+            {classModal}
+        </PageTemplate>
+    )
+}
+
+
+function ClassHelper(props) {
+    return <>
+        <Button
+            onClick={() => props.onClick(props.class)}>
+            {props.class}
+        </Button>
+        Last Updated: {props.lastUpdated}
+    </>
+}
